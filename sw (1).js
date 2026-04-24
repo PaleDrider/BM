f0ae8025f30dccb89aa3777dc92b798776b74837
PaleDrider <rapsis0114@gmail.com>
2026-04-24 09:04:54 +0300

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,4 @@
+const CACHE='blueprint-v2';
+const FILES=['./blueprint-maker.html','./manifest.json'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
